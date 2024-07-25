3f963868ea33ed09514f0c6683dbb70249ef1f08
AkshataDM <oxta28@gmail.com>
2024-07-24 22:28:13 -0500

    Formatting changes

--- agents.py
+++ agents.py
@@ -2,7 +2,7 @@ from textwrap import dedent
 from crewai import Agent, Process
 from langchain.llms import Ollama
 
-# from tools.ExaSearchTool import ExaSearchTool
+
 from langchain_community.tools import DuckDuckGoSearchRun
 
 ollama_model = Ollama(model="llama3")
@@ -13,7 +13,9 @@ class PreparationAgents():
 		return Agent(
 			role='Research Agent',
 			goal='Gather the latest financial news about the company.',
-			backstory='You are a private equity research agent. You are great at doing background research on the company and finding the latest updates and news.',
+			backstory=dedent("""You are an investment researcher. 
+					         You are great at doing background research on the company and 
+							 finding the latest updates and news."""),
 			tools=[search_tool],
 			verbose=True,
 			memory=True
@@ -23,7 +25,7 @@ class PreparationAgents():
 		return Agent(
 				role='Summarizer Agent',
 				goal='Generate key points about the research.',
-				backstory='Expert in summarizing and generating comprehensive notes.',
+				backstory=dedent("""Expert in summarizing and generating comprehensive notes."""),
 				llm=ollama_model,
 				verbose=True,
 				memory=True,
@@ -33,7 +35,7 @@ class PreparationAgents():
 		return Agent(
 				role='Recommender Agent',
 				goal='Generate recommendation based on summary.',
-				backstory='Expert in providing a recommendation to invest in the company or not.',
+				backstory=dedent("""Expert in providing a recommendation to invest in the company or not."""),
 				llm=ollama_model,
 				verbose=True,
 				memory=True,
